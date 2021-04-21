d1e3655fd0812d0b12a076cadf9d8248c454b1f5
dianaandreea28 <82519056+dianaandreea28@users.noreply.github.com>
2021-04-21 15:44:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('.a').hover(function(){
+        $('.a .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.a .tile').hide();
+    })
+
+
+    $('.b').hover(function(){
+        $('.b .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.b .tile').hide();
+    })
+
+
+    $('.c').hover(function(){
+        $('.c .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.c .tile').hide();
+    })
+
+
+    $('.d').hover(function(){
+        $('.d .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.d .tile').hide();
+    })
+
+
+    $('.e').hover(function(){
+        $('.e .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.e .tile').hide();
+    })
+
+    
+    $('.f').hover(function(){
+        $('.f .tile').show();
+        console.log("hovered")
+    }, function(){
+        $('.f .tile').hide();
+    })
+
+});
